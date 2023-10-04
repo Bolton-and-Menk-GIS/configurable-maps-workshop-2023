238a6ad38e04b1d695629ce6d0dca26e7152ef64
Caleb Mackey <caleb.mackey@gmail.com>
2023-10-04 11:48:57 -0500

    initial bootstrap and ahh header component

--- timeline-mapper/vite.config.ts
+++ timeline-mapper/vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
   }
 })
