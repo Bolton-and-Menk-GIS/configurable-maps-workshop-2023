f93778bc0ed50494a5f51b5f7d1b1a8efc8f8be6
Caleb Mackey <caleb.mackey@gmail.com>
2023-10-04 02:41:53 -0500

    feat: solution code for section 1

---
+++ timeline-mapper/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header>
+    <nav class="navbar app-navbar bg-primary">
+      <div class="container-fluid">
+        <span class="float-right d-flex justify-content-between mt-1">
+          <!-- placeholder for left drawer collapse icon -->
+        </span>
+        <span class="navbar-brand mb-0 h1 text-light">Timeline Mapper</span>
+        
+        <span class="float-right d-flex justify-content-between mt-1">
+           <!-- placeholder for right drawer collapse icon and dark mode toggle -->
+        </span>
+      </div>
+    </nav>
+  </header>
+</template>
--- timeline-mapper/vite.config.ts
+++ timeline-mapper/vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
   }
 })
